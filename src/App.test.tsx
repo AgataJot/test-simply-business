ed938849856ea28de4f23864004361b70edd4407
Agata Jewko <agata@zero-1.co>
2019-05-21 16:12:51 +0100

    Add test for todo list item

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -19,54 +19,62 @@ describe("Todo List App", () => {
         ReactDOM.unmountComponentAtNode(div);
     });
 
-    it("shows profile pic and username", () => {
-        const sidebar = app.find(Sidebar).dive();
+    describe("sidebar", () => {
+        it("shows profile pic and username", () => {
+            const sidebar = app.find(Sidebar).dive();
 
-        expect(sidebar.contains(<span>Agata Jewko</span>)).toEqual(true);
-        expect(
-            sidebar.contains(
-                <img src="http://placekitten.com/g/50/50" alt="Agata Jewko" />,
-            ),
-        ).toEqual(true);
+            expect(sidebar.contains(<span>Agata Jewko</span>)).toEqual(true);
+            expect(
+                sidebar.contains(
+                    <img
+                        src="http://placekitten.com/g/50/50"
+                        alt="Agata Jewko"
+                    />,
+                ),
+            ).toEqual(true);
+        });
+
+        it("shows an area containing a list of todo lists. This area has 1 fixed list, which is titled: 'Team To-Do List'", () => {
+            const sidebar = app.find(Sidebar).dive();
+            const todoListUl = sidebar.find("ul");
+            const todoListLi = todoListUl.find("li");
+
+            expect(todoListUl).toHaveLength(1);
+            expect(todoListLi).toHaveLength(1);
+            expect(todoListLi.contains("Team To-Do List")).toEqual(true);
+        });
     });
 
-    // TODO
-    it("does not allow logout", () => {
-        const sidebar = app.find(Sidebar).dive();
-        expect(sidebar.contains("Log out")).toEqual(false);
+    describe("todos list", () => {
+        it('shows all of user\'s current todo list items in my "Team To-Do List​" list', () => {
+            const todos = app.find(TodoList).dive();
+
+            expect(app.contains(<h1>Team To-Do List</h1>)).toEqual(true);
+            expect(todos.find("li")).toHaveLength(4);
+        });
+
+        it('shows empty list in my "Team To-Do List​" list, if none exist', () => {
+            const data = { ...appData(), todos: [] };
+            app = shallow(<App data={data} />);
+            const todos = app.find(TodoList).dive();
+
+            expect(app.contains(<h1>Team To-Do List</h1>)).toEqual(true);
+            expect(todos.find("li")).toHaveLength(0);
+        });
+
+        it("displays items' title", () => {
+            const todos = app.find(TodoList).dive();
+
+            const firstTitle = todos
+                .find("li")
+                .first()
+                .children();
+
+            expect(firstTitle.contains(appData().todos[0].title)).toEqual(true);
+        });
+
+        it("allows adding new todo items to my Team To-Do List​ list", () => {});
+        it("allows delete todo items from my Team To-Do List list", () => {});
+        it("can mark todo items on my “​Team To-Do List​” as complete, and they should visually delineate themselves as being different from non-completed items.", () => {});
     });
-
-    it("shows an area containing a list of todo lists. This area has 1 fixed list, which is titled: 'Team To-Do List'", () => {
-        const sidebar = app.find(Sidebar).dive();
-        const todoListUl = sidebar.find("ul");
-        const todoListLi = todoListUl.find("li");
-
-        expect(todoListUl).toHaveLength(1);
-        expect(todoListLi).toHaveLength(1);
-        expect(todoListLi.contains("Team To-Do List")).toEqual(true);
-    });
-
-    // TODO
-    it("does not allow add or delete the list of todo lists, including amending their titles", () => {
-        const sidebar = app.find(Sidebar).dive();
-        const todoListUl = sidebar.find("ul");
-        const todoListLi = todoListUl.find("li");
-
-        expect(sidebar.contains("Add")).toEqual(false);
-        expect(todoListLi.contains("Delete")).toEqual(false);
-        expect(todoListLi.contains(<input />)).toEqual(false);
-    });
-
-    it('shows all of user\'s current todo list items in my "Team To-Do List​" list', () => {
-        const todos = app.find(TodoList).dive();
-
-        expect(app.contains(<h1>Team To-Do List</h1>)).toEqual(true);
-        expect(todos.find(<li />)).toHaveLength(4);
-    });
-
-    it('shows empty list in my "Team To-Do List​" list, if none exist', () => {});
-
-    it("allows adding new todo items to my Team To-Do List​ list", () => {});
-    it("allows delete todo items from my Team To-Do List list", () => {});
-    it("can mark todo items on my “​Team To-Do List​” as complete, and they should visually delineate themselves as being different from non-completed items.", () => {});
 });
