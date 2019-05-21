2c272f709ae7ff8bd663e05649e23c52b57f4057
Agata Jewko <agata@zero-1.co>
2019-05-21 13:20:30 +0100

    App barebones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import TodoList from "./TodoList";
+import moment from "moment";
+
+const AppSt = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+`;
+
+const MainSectionSt = styled.section`
+    flex: 1 1 calc(100% - 200px);
+`;
+
+const HeaderSt = styled.header`
+    background-color: #581de8;
+    min-height: 200px;
+    width: 100%;
+    padding: 20px;
+    color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    & > h1 {
+        font-size: 2rem;
+        font-weight: 200;
+    }
+`;
+
 function App() {
-    return null;
+    return (
+        <AppSt>
+            <Sidebar />
+            <MainSectionSt>
+                <HeaderSt>
+                    <h1>Team To-Do List</h1>
+                    <time>{moment().format()}</time>
+                </HeaderSt>
+                <TodoList />
+            </MainSectionSt>
+        </AppSt>
+    );
 }
 
 export default App;
